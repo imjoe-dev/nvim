fd10304e36ad4951b02918ec8b4a88129500dbc6
Jose Lopez Ariza <lopezarizajosealejandro@gmail.com>
2025-07-30 23:01:31 -0500

    styles: format

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,14 +1,14 @@
 return {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-        local autopairs = require("nvim-autopairs")
-        autopairs.setup({})
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	config = function()
+		local autopairs = require("nvim-autopairs")
+		autopairs.setup({})
 
-        local cmp_status, cmp = pcall(require, "cmp")
-        if cmp_status then
-            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-        end
-    end,
+		local cmp_status, cmp = pcall(require, "cmp")
+		if cmp_status then
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end
+	end,
 }
