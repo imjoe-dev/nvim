9082400a679b14fbd3947c62fb96f20f94da47e5
Jose Lopez <67883887+imjoe-dev@users.noreply.github.com>
2025-07-30 22:51:00 -0500

    Add mini.surround and autopairs plugins

---
+++ lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,15 @@
+return {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        dependencies = { "hrsh7th/nvim-cmp" },
+        config = function()
+                local autopairs = require("nvim-autopairs")
+                autopairs.setup({ check_ts = true })
+
+                local cmp_status, cmp = pcall(require, "cmp")
+                if cmp_status then
+                        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+                        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+                end
+        end,
+}
