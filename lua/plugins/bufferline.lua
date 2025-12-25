4b2c014cb36d882cad0aea4dea3c124ace758ebb
Jose Lopez Ariza <lopezarizajosealejandro@gmail.com>
2025-12-25 14:51:22 -0500

    feat (bufferline): add keys to bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,6 +2,16 @@ return {
 	"akinsho/bufferline.nvim",
 	version = "*",
 	event = { "VeryLazy" },
+	keys = {
+		{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "toggle pin" },
+		{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "delete non pinned" },
+		{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "close right" },
+		{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "close left" },
+		{ "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", desc = "prev buffer" },
+		{ "<S-l>", "<Cmd>BufferLineCycleNext<CR>", desc = "next buffer" },
+		{ "[B", "<Cmd>BufferLineMovePrev<CR>", desc = "move buffer prev" },
+		{ "]B", "<Cmd>BufferLineMoveNext<CR>", desc = "move buffer next" },
+	},
 	opts = {
 		options = {
 			offsets = {
