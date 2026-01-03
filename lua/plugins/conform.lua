db03e99ac8e59e9f03e901268f3543f222b96416
Jose Lopez Ariza <lopezarizajosealejandro@gmail.com>
2026-01-03 12:13:09 -0500

    fix (treesitter): fixed

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -15,6 +15,7 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
+			go = { "gofumpt" },
 		},
 		format_on_save = { timeout_ms = 500 },
 	},
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,35 +2,96 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	branch = "main",
 	version = false,
-	event = { "BufReadPre", "BufNewFile" },
+	lazy = false,
 	build = ":TSUpdate",
 	dependencies = {
 		{ "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
 	},
 	config = function()
-		local ts = require("nvim-treesitter")
+		require("nvim-treesitter").setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
 
-		ts.install({ "lua", "go", "gomod", "gosum" })
+		require("nvim-treesitter").install({
+			-- go
+			"go",
+			"gomod",
+			"gosum",
+			"gowork",
+
+			-- react/js
+			"typescript",
+			"tsx",
+			"javascript",
+			"jsdoc",
+
+			-- json
+			"json",
+			"jsonc",
+
+			"sql",
+			"html",
+			"css",
+			"markdown",
+			"markdown_inline",
+			"lua",
+			"luadoc",
+			"query",
+		})
 
 		vim.api.nvim_create_autocmd("FileType", {
-			group = vim.api.nvim_create_augroup("ui.treesitter", { clear = true }),
-			pattern = { "*" },
-			callback = function(e)
-				local lang = e.match
-
-				local ok, _ = pcall(ts.install, { lang }, { summary = true })
-				if not ok then
-					return
-				end
-
-				ok, _ = pcall(vim.treesitter.start, e.buf, lang)
-				if not ok then
-					return
-				end
-
-				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+			pattern = {
+				"go",
+				"gomod",
+				"gosum",
+				"gowork",
+				"typescript",
+				"typescriptreact",
+				"javascript",
+				"javascriptreact",
+				"json",
+				"jsonc",
+				"sql",
+				"html",
+				"css",
+				"markdown",
+			},
+			callback = function()
+				vim.treesitter.start()
 			end,
 		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"go",
+				"typescript",
+				"typescriptreact",
+				"javascript",
+				"javascriptreact",
+				"json",
+				"sql",
+			},
+			callback = function()
+				vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				vim.wo[0][0].foldmethod = "expr"
+				vim.wo[0][0].foldlevel = 99
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"go",
+				"typescript",
+				"typescriptreact",
+				"javascript",
+				"javascriptreact",
+			},
+			callback = function()
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+
+		vim.treesitter.language.register("tsx", { "typescriptreact" })
+		vim.treesitter.language.register("javascript", { "javascriptreact" })
 	end,
 }
