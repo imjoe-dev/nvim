85c6a2b8be8fd784a749df7f1bae530b982ae597
Jose Lopez Ariza <lopezarizajosealejandro@gmail.com>
2026-01-03 19:39:48 -0500

    fix (bufferline): close others

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,7 +7,7 @@ return {
 		{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "delete non pinned" },
 		{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "close right" },
 		{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "close left" },
-		{ "<leader>bo", "<Cms>BufferLineCloseOthers<CR>", desc = "close others" },
+		{ "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "close others" },
 		{ "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", desc = "prev buffer" },
 		{ "<S-l>", "<Cmd>BufferLineCycleNext<CR>", desc = "next buffer" },
 		{ "[B", "<Cmd>BufferLineMovePrev<CR>", desc = "move buffer prev" },
